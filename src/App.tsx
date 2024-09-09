65dca1e94f1332330b6b06f77c2d5addca7a8f33
hans <hans.wellenschritt@redcare-pharmacy.com>
2024-09-09 12:01:39 +0200

    5.3.0 reproduce typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import {Configure} from "react-instantsearch";
-import {SearchClient} from "algoliasearch";
+import {Configure, InstantSearch} from "react-instantsearch";
+import {SearchClient, algoliasearch} from "algoliasearch";
 
 function App() {
   // this rule fails here @typescript-eslint/no-redundant-type-constituents
-  const foo: SearchClient | null = null
-  // this rule fails here @typescript-eslint/no-unsafe-assignmen
-  const bar = foo
-
-  console.log(bar)
+  const searchClient: SearchClient = algoliasearch('appId', 'apiKey')
 
   return (
-    <>
+    <InstantSearch
+      indexName="indexName"
+      searchClient={searchClient}
+    >
       <Configure
         // types for this properties are not defined
         hitsPerPage={3}
         analyticsTags={['suggestions']}
       />
-    </>
+    </InstantSearch>
   )
 
 }
