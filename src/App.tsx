22bb1387d85e3723d244a55243d63b95b9982bd0
hans <hans.wellenschritt@redcare-pharmacy.com>
2024-09-09 12:07:28 +0200

    5.3.0 reproduce typing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {InstantSearch} from "react-instantsearch";
 import {SearchClient, algoliasearch} from "algoliasearch";
 
 function App() {
-  // this rule fails here @typescript-eslint/no-redundant-type-constituents
   const searchClient: SearchClient = algoliasearch('appId', 'apiKey')
 
   return (
